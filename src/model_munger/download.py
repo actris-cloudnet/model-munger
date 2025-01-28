076fe4b13b90c82e8a68b2820a5cf00dd85aa599
Tuomas Siipola <tuomas.siipola@fmi.fi>
2025-01-28 11:31:23 +0200

    Retry failed download

--- src/model_munger/download.py
+++ src/model_munger/download.py
@@ -1,5 +1,6 @@
 import datetime
 import sys
+import time
 from pathlib import Path
 from typing import Literal
 
@@ -40,22 +41,50 @@ def download_ecmwf(
         if path.exists():
             continue
         url = f"{base_url}/{date_str}/{run_str}z/ifs/0p25/{stream}/{filename}"
-        _download_file(url, path)
+        _download_file_with_retry(url, path)
     return paths
 
 
+def _download_file_with_retry(url: str, out: Path):
+    attempt = 0
+    while True:
+        try:
+            _download_file(url, out)
+            break
+        except requests.HTTPError as e:
+            print(
+                f"Failed to download file on attempt {attempt+1}: {e}", file=sys.stderr
+            )
+            out.unlink(missing_ok=True)
+            if attempt > 10:
+                raise
+            time.sleep(2**attempt)
+        attempt += 1
+
+
 def _download_file(url: str, out: Path):
-    with out.open("wb") as f, requests.get(url, stream=True) as res:
-        res.raise_for_status()
-        total_bytes = res.headers.get("Content-Length")
-        if total_bytes is None:
-            f.write(res.content)
-        else:
-            dl_bytes = 0
-            total_bytes_int = int(total_bytes)
-            for data in res.iter_content(chunk_size=4096):
-                dl_bytes += len(data)
-                f.write(data)
-                percent = round(100 * dl_bytes / total_bytes_int)
-                print(f"\r[{percent:3}%] {url}", end="", file=sys.stderr, flush=True)
-    print(file=sys.stderr)
+    try:
+        pending_output = False
+        with requests.get(url, stream=True) as res:
+            res.raise_for_status()
+            total_bytes = res.headers.get("Content-Length")
+            with out.open("wb") as f:
+                if total_bytes is None:
+                    f.write(res.content)
+                else:
+                    dl_bytes = 0
+                    total_bytes_int = int(total_bytes)
+                    for data in res.iter_content(chunk_size=4096):
+                        dl_bytes += len(data)
+                        f.write(data)
+                        percent = round(100 * dl_bytes / total_bytes_int)
+                        print(
+                            f"\r[{percent:3}%] {url}",
+                            end="",
+                            file=sys.stderr,
+                            flush=True,
+                        )
+                        pending_output = True
+    finally:
+        if pending_output:
+            print(file=sys.stderr)
